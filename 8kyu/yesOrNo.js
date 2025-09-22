4318c558c11be9f035a9d2ad9d4feb2c502626b5
dimaBurkevich <assassin541@mail.ru>
2025-09-22 14:04:44 +0300

    yesOrNo.js

---
+++ 8kyu/yesOrNo.js
@@ -0,0 +1,14 @@
+
+ // ❓DESCRIPTIONS
+ // Complete the method that takes a boolean value and return
+ // a "Yes" string for true, or a "No" string for false.
+ // 👌TESTS
+ // assert.strictEqual(boolToWord(true), 'Yes')
+ // assert.strictEqual(boolToWord(false), 'No')
+
+ // 😤SOLUTION
+ function boolToWord( bool ){
+     return bool ? 'Yes': 'No'
+ }
+
+ console.log(boolToWord(true))
