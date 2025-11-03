095b131169e7a1dc539ac8d95dbc7635197b2eb2
dimaBurkevich <assassin541@mail.ru>
2025-11-03 20:34:40 +0300

    are_You_Playing_Banjo.js

---
+++ 8kyu/are_You_Playing_Banjo.js
@@ -0,0 +1,32 @@
+
+ // ❓DESCRIPTIONS
+ //Create a function which answers the question "Are you playing banjo?".
+ // If your name starts with the letter "R" or lower case "r", you are playing banjo!
+ //
+ // The function takes a name as its only argument, and returns one of the following strings:
+ //
+ // name + " plays banjo"
+ // name + " does not play banjo"
+ // Names given are always valid strings.
+
+ // 👌TESTS
+//   assert.strictEqual(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+ //     assert.strictEqual(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+ //     assert.strictEqual(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+ //     assert.strictEqual(areYouPlayingBanjo("bravo"), "bravo does not play banjo");
+ //     assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
+ // 😤SOLUTION
+ function areYouPlayingBanjo(name) {
+const result = name[0]
+if (result.toLowerCase() === 'r') {
+    return  name + " plays banjo"
+} else {
+    return name + " does not play banjo"
+}
+
+
+     // Implement me
+
+ }
+
+ console.log(areYouPlayingBanjo("ingo"))
