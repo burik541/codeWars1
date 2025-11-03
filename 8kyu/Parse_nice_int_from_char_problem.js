ffd402fa6593f93a328a3ea5ae92e3d526344491
dimaBurkevich <assassin541@mail.ru>
2025-11-03 20:35:14 +0300

    Parse_nice_int_from_char_problem.js

---
+++ 8kyu/Parse_nice_int_from_char_problem.js
@@ -0,0 +1,23 @@
+
+ // ❓DESCRIPTIONS
+ //You ask a small girl "How old are you?" She always says "x years old",
+ // where x is a random number between 0 and 9.
+ //
+ // Write a program that returns the girl's age (0-9) as an integer.
+ //
+ // Assume the test input string is always a valid string.
+ // For example, the test input may be "1 year old" or "5 years old".
+ // The first character in the string is always a number.
+
+ // 👌TESTS
+//  assert.strictEqual(getAge("4 years old"), 4);
+ //     assert.strictEqual(getAge("9 years old"), 9);
+ //     assert.strictEqual(getAge("1 year old"), 1);
+ // 😤SOLUTION
+ function getAge(inputString){
+
+    return Number(inputString[0]);
+// return the girl's correct age as an integer. Happy coding :)
+ }
+
+ console.log(getAge('9 years old'))
